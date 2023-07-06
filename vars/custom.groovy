a8dbfc6e4b02c4ae4038def84a3fc6ebd0a6e909
Mr. Mohment <134716386+MrMohment@users.noreply.github.com>
2023-07-06 17:54:42 -0400

    Update custom.groovy

--- vars/custom.groovy
+++ vars/custom.groovy
@@ -2,17 +2,17 @@ def call(String stageName){
 
   if ("${stageName}" == "Build"){
           sh "echo Starting the build process"
-			    sh "mvn clean package" 
-			    sh "echo Build is now complete"
+	  sh "mvn clean package" 
+	  sh "echo Build is now complete"
   }
   if ("${stageName}" == "CodeQuality"){
         sh "echo Now performing Code Quality Analysis"
-				sh "mvn sonar:sonar"
-				sh "echo Code Quality Analysis complete"
+	sh "mvn sonar:sonar"
+	sh "echo Code Quality Analysis complete"
   }
   if ("${stageName}" == "UploadArtifacts"){
         sh "echo Uploading artifacts to Nexus Repo"
-				sh "mvn deploy"
-				sh "echo Upload complete"
+	sh "mvn deploy"
+	sh "echo Upload complete"
   }
-}
+}  //Close of def 
